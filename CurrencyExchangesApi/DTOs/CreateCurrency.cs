f4a3795ab1bf2e59ee3bbb7e73ceee3023ae27f4
Vladislav Kostin <vladislavkostin.ds@yandex.ru>
2023-11-04 16:15:48 +0300

    add annotations to CreateCurrrencyDto

--- CurrencyExchangesApi/DTOs/CreateCurrency.cs
+++ CurrencyExchangesApi/DTOs/CreateCurrency.cs
@@ -4,8 +4,14 @@ namespace CurrencyExchangesApi.DTOs
 {
     public class CreateCurrency
     {
+        [Required]
+        [StringLength(3, MinimumLength = 3)]
         public string Code { get; set; } = string.Empty;
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public string Sign { get; set; } = string.Empty;
     }
 }
